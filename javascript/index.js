cf6ee2d7e1a837553302cf76f4a20f9422ca9917
salomeIsCoding <160015952+salomeIsCoding@users.noreply.github.com>
2024-05-23 22:22:26 +0200

    Added Javascript function updateTime

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,20 @@
-let parisElement = document.querySelector("#paris");
-let parisDateElement = parisElement.querySelector(".date");
-parisDateElement.innerHTML = "HI";
+function updateTime(){
+    // Paris
+    let parisElement = document.querySelector("#paris");
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+    
+    // London
+    let londonElement = document.querySelector("#london");
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonTime = moment().tz("Europe/London");
+    londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+    londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000)
